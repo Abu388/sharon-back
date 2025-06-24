dc0c83bd3125cdda4b642fec48be4026f0cad280
yabbby <ye.aba.net@gmail.com>
2025-06-24 22:21:43 +0300

    update on donation

--- back-app/app/config.py
+++ back-app/app/config.py
@@ -7,6 +7,8 @@ class Config:
     )
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = os.getenv('SECRET_KEY', 'yabbby')  # Default to 'yabbby'
+    UPLOAD_FOLDER = 'uploads/receipts'
+    MAX_CONTENT_LENGTH = 5 * 1024 * 1024  # 5MB file limit
 
 class DevelopmentConfig(Config):
     DEBUG = True
